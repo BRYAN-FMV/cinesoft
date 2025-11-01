4d32f998503dad9f13c638a7ae6cc1919603735d
nery gonzalez <nerymaradiaga161021@outlook.com>
2025-10-31 18:37:08 -0600

    Actualizacion de Login y Register

--- cinesoft-app/src/main.jsx
+++ cinesoft-app/src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Cartelera from './PANTALLAS/Cartelera/cartelera.jsx'
 import DetallePelicula from './PANTALLAS/DetallePelicula/DetallePelicula.jsx'; 
-import Cartelera from './Cartelera/cartelera.jsx'
-import DetallePelicula from './DetallePelicula/DetallePelicula.jsx'; 
+//import Cartelera from './Cartelera/cartelera.jsx'
+//import DetallePelicula from './DetallePelicula/DetallePelicula.jsx'; 
 import Login from './Auth/Login.jsx'
 import Register from './Auth/Register.jsx'
 
