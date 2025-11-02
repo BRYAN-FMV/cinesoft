25e6dad30df39ca1c0269ccdf193e61985b78df5
nery gonzalez <nerymaradiaga161021@outlook.com>
2025-11-02 17:19:11 -0600

    Creacion de la animacion

--- cinesoft-app/src/main.jsx
+++ cinesoft-app/src/main.jsx
@@ -6,16 +6,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Cartelera from './PANTALLAS/Cartelera/cartelera.jsx'
 import DetallePelicula from './PANTALLAS/DetallePelicula/DetallePelicula.jsx'; 
-//import Cartelera from './Cartelera/cartelera.jsx'
-//import DetallePelicula from './DetallePelicula/DetallePelicula.jsx'; 
 import Login from './Auth/Login.jsx'
 import Register from './Auth/Register.jsx'
+import SplashScreen from './components/SplashScreen/SplashScreen.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Cartelera />} />
+        <Route path="/" element={<SplashScreen />} />
+        <Route path="/cartelera" element={<Cartelera />} />
         <Route path="/detalle/:id" element={<DetallePelicula />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
